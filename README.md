66fb9657c2b67441b754dcaca8833930a16a1d8c
Osama Sultan <80481444+osamaBenSultan@users.noreply.github.com>
2026-03-18 14:52:12 +0300

    init: initial project structure and directory layout

---
+++ README.md
@@ -0,0 +1,23 @@
+# Study Track WebSite (Bonyan - Task 02)
+
+## Overview
+
+### Second Project in the **Laravel Internship Training** with **Bonyan Initiative** 🚀
+
+-
+-
+
+### Key Features of the Project:
+
+-
+-
+
+
+
+### Tech Stack
+
+-
+-
+
+
+---
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Study Track</title>
+    <link rel="icon" type="image/png" href="images/study-icon1.png" sizes="any">
+
+</head>
+<body>
+    <h1>Home Page</h1>
+</body>
+</html>
